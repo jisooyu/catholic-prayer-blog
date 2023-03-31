8a5d804cc17712e9361c58d78bfd7c1244dee8ce
jisooyu <jisooyu@hotmail.com>
2023-03-31 10:54:30 +0900

    setting engines in the package.json to dispatch it to Heroku

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
