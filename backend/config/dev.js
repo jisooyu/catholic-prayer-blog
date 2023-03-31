3ef7e322cfa494da8d12d78cc9c7987aec97e310
jisooyu <jisooyu@hotmail.com>
2023-03-31 14:15:24 +0900

    adding heroku-postbuild

---
+++ backend/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '298593507104-i1d3ts0488926r47fe5o6o43e0koe5hh.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://jisooyu:EdfcDygdcYrsebA7@yu-web.fbm0a.mongodb.net/yu-supporting-desk?retryWrites=true&w=majority',
+    secretKey: 'jlkjfdklajfdajiejijijfdf',
+}
