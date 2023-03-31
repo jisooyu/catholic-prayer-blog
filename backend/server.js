f89fc8f67629027324b30c9631fa8348f6dc72e3
jisooyu <jisooyu@hotmail.com>
2023-03-31 14:11:17 +0900

    npm run build at the frontend and adding if statement for production in backend express js

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,22 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+
+if (process.env.NODE_ENV = 'production') {
+    /*
+    Express will server up production assets
+    like our main.js or main.css files
+    */
+    app.use(express.static('client/build'));
+
+    // Express will serve up index.html file
+    // if it doen't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
