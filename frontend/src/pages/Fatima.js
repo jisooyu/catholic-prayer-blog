fd57024f4e087d15ef09a017a877a62c72dc8dff
jisooyu <jisooyu@hotmail.com>
2023-04-02 15:55:56 +0900

    trying to fix Fatima

--- frontend/src/pages/Fatima.js
+++ frontend/src/pages/Fatima.js
@@ -4,15 +4,15 @@ import { Card, CardMedia, CardContent, CardHeader } from '@mui/material'
 const Fatima = () => {
     return (
         <div style={{ marginTop: '70px', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{ maxWidth: 780 }}>
                 <CardHeader
                     title="Holy Mother in Fatima"
                     subheader="1917"
                 />
                 <CardMedia
                     component="img"
-                    height="620"
-                    image={`${process.env.PUBLIC_URL}/fatima-mother.jpeg`}
+                    height="460"
+                    image={`${process.env.PUBLIC_URL}/Lords-Prayer.jpeg`}
                 />
                 <CardContent>
 
