10913d47fff65c360dbc584d7c0f159da7f25b5e
jisooyu <jisooyu@hotmail.com>
2023-04-02 20:06:09 +0900

    testing faust

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ require('./routes/authRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
     const path = require('path');
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
     app.get('/faust', (req, res) => {
