aea13a4def461b27eadd6dacd566dd6f4a39cc77
jisooyu <jisooyu@hotmail.com>
2023-04-04 21:01:09 +0900

    90vh

--- frontend/src/components/PrePrayer.js
+++ frontend/src/components/PrePrayer.js
@@ -9,10 +9,10 @@ const PrePrayer = ({ prePrayer }) => {
         alignItems: 'center',
         justifyContent: 'center',
         width: isIphone13 ? '75vw' : '60vw',
-        marginTop: isIphone13 ? '2em' : '5em',
+        marginTop: isIphone13 ? '1em' : '5em',
         marginLeft: isIphone13 ? '1em' : '5em',
-        height: isIphone13 ? '85vh' : '80vh',
-        paddingTop: isIphone13 ? '8em' : '3em',
+        height: isIphone13 ? '90vh' : '80vh',
+        paddingTop: isIphone13 ? '3em' : '1em',
         overflow: 'scroll',
         backgroundColor: isIphone13 ? "#f95959" : "#38598b"
     };
