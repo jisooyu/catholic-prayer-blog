2a831ae577d6e68bd2c05f15fd50a5d39be2f2f0
jisooyu <jisooyu@hotmail.com>
2023-04-04 21:07:38 +0900

    marginTop 2em

--- frontend/src/components/PrePrayer.js
+++ frontend/src/components/PrePrayer.js
@@ -9,7 +9,7 @@ const PrePrayer = ({ prePrayer }) => {
         alignItems: 'center',
         justifyContent: 'center',
         width: isIphone13 ? '75vw' : '60vw',
-        marginTop: isIphone13 ? '1em' : '5em',
+        marginTop: isIphone13 ? '2em' : '5em',
         marginLeft: isIphone13 ? '1em' : '5em',
         height: isIphone13 ? '90vh' : '80vh',
         paddingTop: isIphone13 ? '3em' : '1em',
