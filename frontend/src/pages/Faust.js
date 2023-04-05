d0c68c9ef975a4c433511a94f01599f2107b7b25
jisooyu <jisooyu@hotmail.com>
2023-04-05 20:46:07 +0900

    setting the height and the width again

--- frontend/src/pages/Faust.js
+++ frontend/src/pages/Faust.js
@@ -21,7 +21,7 @@ const Faust = () => {
             />
             <CardMedia
                 sx={{
-                    height: isIphone13 ? 400 : 800,
+                    height: isIphone13 ? 500 : 1000,
                     width: isIphone13 ? 300 : 600
                 }}
                 component="img"
