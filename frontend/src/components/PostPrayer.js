a3a8e6a5de49a2fbd9249e746dcd302e12e5da6a
jisooyu <jisooyu@hotmail.com>
2023-04-11 15:53:39 +0900

    removing sx paperStyle

--- frontend/src/components/PostPrayer.js
+++ frontend/src/components/PostPrayer.js
@@ -17,28 +17,26 @@ const PostPrayer = ({ postPrayer }) => {
     };
 
     return (
-        <div style={{ marginTop: '4em' }}>
-            <Paper
-                elevation={0}
-                sx={paperStyle}
-            >
-                <Grid container spacing={1}>
-                    <Grid item xs container direction="column" spacing={1}>
-                        <Grid item xs sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "60vh" }}>
-                            <Typography gutterBottom variant="h5" component="div" style={{ textAlign: "center" }}>
-                                {postPrayer.title}
-                            </Typography>
-                            <Typography variant="subtitle1" gutterBottom>
-                                {postPrayer.content}
-                            </Typography>
-                            <Typography variant="h6" gutterBottom style={{ textAlign: "center" }}>
-                                {postPrayer.ending}
-                            </Typography>
-                        </Grid>
+        <Paper
+            elevation={0}
+        // sx={paperStyle}
+        >
+            <Grid container spacing={1}>
+                <Grid item xs container direction="column" spacing={1}>
+                    <Grid item xs sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "60vh" }}>
+                        <Typography gutterBottom variant="h5" component="div" style={{ textAlign: "center" }}>
+                            {postPrayer.title}
+                        </Typography>
+                        <Typography variant="subtitle1" gutterBottom>
+                            {postPrayer.content}
+                        </Typography>
+                        <Typography variant="h6" gutterBottom style={{ textAlign: "center" }}>
+                            {postPrayer.ending}
+                        </Typography>
                     </Grid>
                 </Grid>
-            </Paper >
-        </div>
+            </Grid>
+        </Paper >
     );
 }
 export default PostPrayer;
