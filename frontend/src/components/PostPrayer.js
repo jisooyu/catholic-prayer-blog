ba7ddaa5d98cc172e073b4e3316cfce46b6108b3
jisooyu <jisooyu@hotmail.com>
2023-04-11 15:58:41 +0900

    creating gridStyle

--- frontend/src/components/PostPrayer.js
+++ frontend/src/components/PostPrayer.js
@@ -8,22 +8,24 @@ const PostPrayer = ({ postPrayer }) => {
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
+
+    };
+    const gridStyle = {
         marginTop: isIphone13 ? '1em' : '2em',
         marginLeft: isIphone13 ? '2em' : '5em',
         height: isIphone13 ? '80vh' : '35vh',
         width: isIphone13 ? '82vw' : '50vw',
         paddingTop: isIphone13 ? '4em' : '1em',
         overflow: 'scroll'
-    };
-
+    }
     return (
         <Paper
             elevation={0}
-        // sx={paperStyle}
+            sx={paperStyle}
         >
             <Grid container spacing={1}>
                 <Grid item xs container direction="column" spacing={1}>
-                    <Grid item xs sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "60vh" }}>
+                    <Grid item xs sx={gridStyle}>
                         <Typography gutterBottom variant="h5" component="div" style={{ textAlign: "center" }}>
                             {postPrayer.title}
                         </Typography>
