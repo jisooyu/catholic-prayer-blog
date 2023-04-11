3a5603788152f89cacc3d5bbb08a0261d9e64f4b
jisooyu <jisooyu@hotmail.com>
2023-04-11 16:38:41 +0900

    removing gutterBottom

--- frontend/src/components/PostPrayer.js
+++ frontend/src/components/PostPrayer.js
@@ -23,19 +23,24 @@ const PostPrayer = ({ postPrayer }) => {
     return (
         <Paper elevation={0} sx={paperStyle}>
             <Grid container spacing={0.01}>
-                <Grid item xs container direction="column" spacing={1}>
-                    <Grid item xs sx={gridStyle}>
-                        <Typography gutterBottom variant="h5" component="div" style={{ textAlign: "left" }}>
+                <Grid item xs container direction="column" spacing={1} sx={gridStyle}>
+                    <Grid item xs>
+                        <Typography variant="h5" component="div" style={{ textAlign: "center" }}>
                             {postPrayer.title}
                         </Typography>
-                        <Typography variant="subtitle1" gutterBottom>
+                    </Grid>
+                    <Grid>
+                        <Typography variant="subtitle1" style={{ textAlign: "left" }} >
                             {postPrayer.content}
                         </Typography>
-                        <Typography variant="h6" gutterBottom style={{ textAlign: "left" }}>
+                    </Grid>
+                    <Grid>
+                        <Typography variant="h6" style={{ textAlign: "center" }}>
                             {postPrayer.ending}
                         </Typography>
                     </Grid>
                 </Grid>
+
             </Grid>
         </Paper>
     );
