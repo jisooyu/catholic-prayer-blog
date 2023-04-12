769af0682bf6b3fe9978d1e8dcd6a3c750389444
jisooyu <jisooyu@hotmail.com>
2023-04-12 21:39:30 +0900

    tweaking Prayer, PrePrayer

--- frontend/src/components/Prayer.js
+++ frontend/src/components/Prayer.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { styled, Grid, Paper, Typography, ButtonBase, useMediaQuery } from '@mui/material'
+import { cardStyle } from '../styles/cardStyle';
 
 const Prayer = ({ prayer }) => {
     const isIphone13 = useMediaQuery('(max-width: 400px)');
@@ -10,16 +11,10 @@ const Prayer = ({ prayer }) => {
         maxHeight: '100%',
     });
     const paperStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: isIphone13 ? '4em' : '2em',
-        marginLeft: isIphone13 ? '2em' : '5em',
+        ...cardStyle,
         height: isIphone13 ? '80vh' : '35vh',
         width: isIphone13 ? '82vw' : '50vw',
-        paddingTop: isIphone13 ? '4em' : '1em',
-        overflow: 'scroll'
+
     };
 
     return (
--- frontend/src/components/PrePrayer.js
+++ frontend/src/components/PrePrayer.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import { Paper, useMediaQuery } from '@mui/material'
-
+import { cardStyle } from '../styles/cardStyle';
 const PrePrayer = ({ prePrayer }) => {
     const isIphone13 = useMediaQuery('(max-width: 400px)');
 
     const paperStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: isIphone13 ? '75vw' : '60vw',
-        marginTop: isIphone13 ? '4em' : '5em',
-        marginLeft: isIphone13 ? '3.5em' : '5em',
+        ...cardStyle,
         height: isIphone13 ? '200vh' : '80vh',
         paddingTop: isIphone13 ? '4em' : '1em',
-        overflow: 'scroll',
     };
 
     const prayerTitle = { textAlign: 'center', marginTop: '20px' }
     const repetitionStyle = { textAlign: 'center', fontWeight: 'bold', marginTop: '20px', marginRight: '30px' }
-    const prayerStyle = { display: 'flex', alignItems: 'center', justifyContent: 'center', marginRight: '5em', marginLeft: '5em', width: '80%' }
+    // const prayerStyle = {
+    //     display: 'flex',
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //     marginRight: '5em',
+    //     marginLeft: '5em',
+    //     width: '80%'
+    // }
 
     return (
         <Paper elevation={0} sx={paperStyle} >
@@ -29,13 +29,13 @@ const PrePrayer = ({ prePrayer }) => {
                 <p style={{ textAlign: 'center', marginTop: '5px', fontWeight: 'bold' }}>{prePrayer.hailMary}</p>
                 {/* 청원기도 */}
                 <h3 style={prayerTitle}>{prePrayer.petitionPrayerTitle}</h3>
-                <div style={prayerStyle}>
+                <div >
                     <p style={{ marginTop: '20px', textAlign: 'left', }}>{prePrayer.petitionPrayer}</p>
                 </div>
                 <h3 style={repetitionStyle}>{prePrayer.repititionOne}</h3>
                 {/* 감사기도 */}
                 <h3 style={prayerTitle}>{prePrayer.thanksgivingPrayerTitle}</h3>
-                <div style={prayerStyle}>
+                <div >
                     <p style={{ marginTop: '20px', textAlign: 'left' }}>{prePrayer.thanksgivingPrayer}</p>
                 </div>
                 <h3 style={repetitionStyle}>{prePrayer.repititionTwo}</h3>
