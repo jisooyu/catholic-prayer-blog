fe1b9c37f2152f9c93cb7a365fab459c1b2f79f2
jisooyu <jisooyu@hotmail.com>
2023-04-12 21:49:52 +0900

    reviving prayerStyle with modification

--- frontend/src/components/PrePrayer.js
+++ frontend/src/components/PrePrayer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Paper, useMediaQuery } from '@mui/material'
 import { cardStyle } from '../styles/cardStyle';
+
 const PrePrayer = ({ prePrayer }) => {
     const isIphone13 = useMediaQuery('(max-width: 400px)');
 
@@ -12,14 +13,14 @@ const PrePrayer = ({ prePrayer }) => {
 
     const prayerTitle = { textAlign: 'center', marginTop: '20px' }
     const repetitionStyle = { textAlign: 'center', fontWeight: 'bold', marginTop: '20px', marginRight: '30px' }
-    // const prayerStyle = {
-    //     display: 'flex',
-    //     alignItems: 'center',
-    //     justifyContent: 'center',
-    //     marginRight: '5em',
-    //     marginLeft: '5em',
-    //     width: '80%'
-    // }
+    const prayerStyle = {
+        // display: 'flex',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        marginRight: '5em',
+        marginLeft: '5em',
+        width: '80%'
+    }
 
     return (
         <Paper elevation={0} sx={paperStyle} >
@@ -29,13 +30,13 @@ const PrePrayer = ({ prePrayer }) => {
                 <p style={{ textAlign: 'center', marginTop: '5px', fontWeight: 'bold' }}>{prePrayer.hailMary}</p>
                 {/* 청원기도 */}
                 <h3 style={prayerTitle}>{prePrayer.petitionPrayerTitle}</h3>
-                <div >
+                <div style={prayerStyle}>
                     <p style={{ marginTop: '20px', textAlign: 'left', }}>{prePrayer.petitionPrayer}</p>
                 </div>
                 <h3 style={repetitionStyle}>{prePrayer.repititionOne}</h3>
                 {/* 감사기도 */}
                 <h3 style={prayerTitle}>{prePrayer.thanksgivingPrayerTitle}</h3>
-                <div >
+                <div style={prayerStyle}>
                     <p style={{ marginTop: '20px', textAlign: 'left' }}>{prePrayer.thanksgivingPrayer}</p>
                 </div>
                 <h3 style={repetitionStyle}>{prePrayer.repititionTwo}</h3>
