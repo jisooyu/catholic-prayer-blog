7dc0c2718bb03eea98ecc400a562acd351742c85
jisooyu <jisooyu@hotmail.com>
2023-04-21 10:36:14 +0900

    adding direction to Grid container

--- frontend/src/components/PostPrayer.js
+++ frontend/src/components/PostPrayer.js
@@ -19,7 +19,7 @@ const PostPrayer = ({ postPrayer }) => {
 
     return (
         <Paper elevation={0} sx={paperStyle}>
-            <Grid container spacing={0.5}>
+            <Grid container spacing={0.5} direction='column'>
                 <Grid item xs={12} sm={3} textAlign="center" >
                     <Typography variant="h5" component="div">
                         {postPrayer.title}
